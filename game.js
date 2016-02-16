b08a275b6793e84499484162b1e501f4fa61f1f2
Tricia Buckenberger <tbuckenberger@gmail.com>
2016-02-16 13:17:41 -0800

    working game

--- game.js
+++ game.js
@@ -1,33 +1,54 @@
 var current = [];
 var allCards = [];
+var totalTries = 0;
+var totalMatched = 0;
+var boardSize;
+var errorMsg = "<h1>Error: This topic is not allowed.  Please choose a different subject.</h1>";
+var testing;
 
-$("button").click(function() {
-  $(".hashtag").hide();
-  var hashtag = $("input").val() || "cats";
+$(".hashtag").click(startGame);
+
+$("input").keydown(function(event){
+  if (event.which === 13) startGame();
+});
+
+$(".reset").click(reset);
+
+function startGame () {
+  boardSize = +$('input[name=GameSize]:checked').val();
+  $(".pregame").hide();
+  var hashtag = $("input.text").val() || "travel";
   var init = $.ajax({
     dataType: "jsonp",
     cache: false,
     url: "https://api.instagram.com/v1/tags/" + hashtag + "/media/recent?client_id=4a059e7a3f9d4870ac229cb32f390a91",
     success: function(data) {
-      var cards = "";
-      var indexNums = [];
-      for(var i = 0; i < data.data.length; i++) {
-        allCards.push(data.data[i].images.thumbnail);
-        indexNums.push(i, i);
+      if (data.meta.code === 200) {
+        var cards = "";
+        var indexNums = [];
+        for(var i = 0; i < boardSize; i++) {
+          allCards.push(data.data[i].images.thumbnail);
+          indexNums.push(i, i);
+        }
+        indexNums = shuffle(indexNums);
+        while(indexNums.length) {
+          cards += "<div class='card'><img class='hidden' src='" + allCards[indexNums.pop()].url + "'></div>";
+        }
+        $("div.game-board").html(cards);
       }
-      indexNums = shuffle(indexNums);
-      while(indexNums.length) {
-        cards += "<div class='back'><img class='hidden' src='" + allCards[indexNums.pop()].url + "'></div>";
-      }
-      $("div.game-board").html(cards);
     }
   });
 
   init.then(function() {
-    listen();
+    if (allCards.length) {
+      $(".gameInProcess").removeClass("hidden");
+      listen();
+    } else {
+      reset();
+      $("div.game-board").html(errorMsg).removeClass("hidden");
+    }
   });
-});
-
+}
 
 //Fisher–Yates Shuffle
 function shuffle(array) {
@@ -42,12 +63,11 @@ function shuffle(array) {
 }
 
 function listen() {
-  $(".back").on("click", function() {
-    $(".back").css("pointer-events", "none");
+  $(".card").on("click", function() {
+    $("body").css("pointer-events", "none");
     $(this).children().removeClass("hidden");
-    current.push($(this).children().attr('src'));
-    $(this).removeClass("back").addClass("clicked");
-      console.log("wtf");
+    current.push($(this).children().last().attr("src"));
+    $(this).removeClass("card").addClass("clicked");
     if(current.length > 1) {
       setTimeout(function(){
         console.log('compare');
@@ -62,33 +82,60 @@ function listen() {
 }
 
 function match() {
+  totalMatched++;
+  $(".matches").html(totalMatched);
   $(".clicked").children().remove();
   $(".clicked").addClass("complete");
   $(".clicked").removeClass("clicked");
+
   current = [];
   console.log("match");
 }
 
 function noMatch() {
   $(".clicked").children().addClass("hidden");
-  $(".clicked").addClass("back").removeClass("clicked");
+  $(".clicked").removeAttr("style");
+  $(".clicked").addClass("card").removeClass("clicked");
   current = [];
   console.log("not a match");
 }
 
 function enable() {
   setTimeout(function(){
-    $(".back").css("pointer-events", "auto");
+    $("body").css("pointer-events", "auto");
+    $(".clicked").css("pointer-events", "none");
   }, 100);
 }
 
 function compare() {
-  if (current[0] == current[1]) {
+  totalTries++;
+  $(".attempts").html(totalTries);
+  if (current[0] === current[1]) {
     match();
   } else {
     noMatch();
   }
+  if (totalMatched === boardSize) {
+    win();
+  }
   enable();
 }
+
+function win() {
+  var winner = '<p class="win">Congratulations!  You won!!</p>';
+  $("div.game-board").html(winner);
+}
+
+function reset() {
+  $("div.game-board").empty();
+  $(".pregame").show();
+  $(".gameInProcess").addClass("hidden");
+  $("input.text").val("");
+  current = [];
+  allCards = [];
+  totalTries = 0;
+  totalMatched = 0;
+}
+
 
 
